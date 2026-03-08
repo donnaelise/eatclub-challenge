18f9cbce6def9b13e526bac4de76410686625605
Donna <donna@giraffe.com.au>
2026-03-08 18:13:40 +1100

    feat: render restaurant list screen with basic card layout and sort sort restaurants by best deal

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -1,1 +1,48 @@
-RestaurantCard.tsx
+import type { Restaurant } from '../types/restaurant'
+import { Heart } from 'lucide-react'
+
+interface RestaurantCardProps {
+  restaurant: Restaurant
+}
+
+export default function RestaurantCard({ restaurant }: RestaurantCardProps) {
+  const bestDeal = restaurant.deals.reduce((best, current) =>
+  Number(current.discount) > Number(best.discount) ? current : best
+)
+  const cuisines = restaurant.cuisines.slice(0, 3).join(', ')
+
+
+  return (
+    <li className="restaurant-card">
+      <div className="restaurant-image-wrap">
+        <img
+          className="restaurant-image"
+          src={restaurant.imageLink}
+          alt={restaurant.name}
+        />
+        {bestDeal && (
+          <div className="deal-badge">
+            <strong>{bestDeal.discount}% off {bestDeal.dineIn === 'true' ? ' - Dine in' : ''}</strong>
+            
+            <span>{bestDeal.close ? `Arrive before ${bestDeal.close}` : 'Anytime today'}</span>
+          </div>
+        )}
+      </div>
+
+      <div className="restaurant-content">
+        <div className="restaurant-title-row">
+          <h2>{restaurant.name}</h2>
+          <Heart size={24} />
+
+        </div>
+        <p className="restaurant-location">
+          {restaurant.suburb}
+        </p>
+        <p className="restaurant-cuisines">{cuisines}</p>
+        <p className="restaurant-hours">
+          {restaurant.open} - {restaurant.close}
+        </p>
+      </div>
+    </li>
+  )
+}
--- src/pages/RestaurantList.tsx
+++ src/pages/RestaurantList.tsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import type { Restaurant } from '../types/restaurant'
 import { useRestaurants } from '../hooks/useRestaurants'
+import RestaurantCard from '../components/RestaurantCard'
 
 export default function RestaurantList() {
-  const { restaurants, loading, error } = useRestaurants()
-  console.log('restaurants:', restaurants)
-  console.log('loading:', loading)
-  console.log('error:', error)
+	const { restaurants, loading, error } = useRestaurants()
+	if(loading) { return(<p>loading...</p>);} 
+	if(error) { return(<p>{error}</p>);}
+	if (restaurants.length === 0) {	return (<p>No restaurants found</p>)}
 
-  return (
-    <>
-    </>
-  )
+		const getBestDiscount = (restaurant: Restaurant) => 
+	Math.max(...restaurant.deals.map(d => Number(d.discount)))
+
+	const sorted = [...restaurants].sort((a, b) => 
+		getBestDiscount(b) - getBestDiscount(a)
+		);
+
+	return (
+		<main className="restaurant-list-page">
+			<ul className="restaurant-list">
+				{sorted.map((restaurant) => (
+					<RestaurantCard
+						key={restaurant.objectId}
+						restaurant={restaurant}
+						/>
+						))}
+			</ul>
+		</main>
+		)
+
 }
